4e646243444372d7607b5444794d11fe212190cc
RayoniR <rayonireuben@duck.com>
2025-09-22 22:45:15 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- network/interfaces/__init__.py
+++ network/interfaces/__init__.py
@@ -1,6 +1,6 @@
-from .connection_interface import IConnectionManager
-from .protocol_interface import IProtocolHandler
-from .discovery_interface import IPeerDiscovery
-from .processor_interface import IMessageProcessor
+from network.interfaces.connection_interface import IConnectionManager
+from network.interfaces.protocol_interface import IProtocolHandler
+from network.interfaces.discovery_interface import IPeerDiscovery
+from network.interfaces.processor_interface import IMessageProcessor
 
 __all__ = ['IConnectionManager', 'IProtocolHandler', 'IPeerDiscovery', 'IMessageProcessor']
