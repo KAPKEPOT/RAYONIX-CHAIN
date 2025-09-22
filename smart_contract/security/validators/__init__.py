7d0ecb68b86ba829da9f374cda9beb041e023053
RayoniR <rayonireuben@duck.com>
2025-09-22 20:10:25 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- smart_contract/security/validators/__init__.py
+++ smart_contract/security/validators/__init__.py
@@ -1,6 +1,6 @@
 # smart_contract/security/validators/__init__.py
-from .input_validator import InputValidator
-from .domain_validator import DomainValidator
-from .ip_validator import IPValidator
+from smart_contract.security.validators.input_validator import InputValidator
+from smart_contract.security.validators.domain_validator import DomainValidator
+from smart_contract.security.validators.ip_validator import IPValidator
 
 __all__ = ['InputValidator', 'DomainValidator', 'IPValidator']
