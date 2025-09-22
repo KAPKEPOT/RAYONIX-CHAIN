a4b98a2e1294311c4f447f4596f78d57d1bb5930
RayoniR <rayonireuben@duck.com>
2025-09-23 02:47:44 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- network/config/__init__.py
+++ network/config/__init__.py
@@ -1,4 +1,4 @@
-'from .node_config import NodeConfig
+from .node_config import NodeConfig
 from network.config.network_types import NetworkType, ProtocolType, ConnectionState, MessageType
 
 __all__ = ['NodeConfig', 'NetworkType', 'ProtocolType', 'ConnectionState', 'MessageType']'
