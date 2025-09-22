138bce99f11aa9c5e5015c50c8cd5e1c83b15a3c
RayoniR <rayonireuben@duck.com>
2025-09-23 02:45:54 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- network/core/__init__.py
+++ network/core/__init__.py
@@ -1,7 +1,7 @@
-from .advanced_p2p_network import AdvancedP2PNetwork
-from .connection_manager import ConnectionManager
-from .peer_discovery import PeerDiscovery
-from .message_processor import MessageProcessor
-from .security_manager import SecurityManager
+from network.core.p2p_network import AdvancedP2PNetwork
+from network.core.connection_manager import ConnectionManager
+from network.core.peer_discovery import PeerDiscovery
+from network.core.message_processor import MessageProcessor
+from network.core.security_manager import SecurityManager
 
 __all__ = ['AdvancedP2PNetwork', 'ConnectionManager', 'PeerDiscovery', 'MessageProcessor', 'SecurityManager']
