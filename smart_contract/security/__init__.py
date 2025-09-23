e141b466a1b8017f5f233074f92ca7e2ae5473fa
RayoniR <rayonireuben@duck.com>
2025-09-23 16:05:51 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- smart_contract/security/__init__.py
+++ smart_contract/security/__init__.py
@@ -1,5 +1,5 @@
 # smart_contract/security/__init__.py
-from smart_contract.contract_security import ContractSecurity
+from smart_contract.security import ContractSecurity
 from smart_contract.security.behavioral_analyzer import BehavioralAnalyzer
 from smart_contract.security.threat_intelligence import ThreatIntelligenceFeed
 from smart_contract.security.validators.input_validator import InputValidator
