f7fca9a3079dcb2d4247a137bf30ca79d60afe0c
RayoniR <rayonireuben@duck.com>
2025-09-23 09:25:38 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- consensusengine/__init__.py
+++ consensusengine/__init__.py
@@ -1,1 +1,23 @@
+# consensus/__init__.py
+"""
+Production-ready Proof-of-Stake Consensus Engine
+"""
 
+__version__ = "1.0.0"
+__author__ = "Consensus Team"
+
+from consensus.core.engine import ProofOfStake
+from consensus.models.validators import Validator, ValidatorStatus
+from consensus.models.blocks import BlockProposal
+from consensus.models.votes import Vote, VoteType
+from consensus.abci.interface import ABCIApplication
+
+__all__ = [
+    'ProofOfStake',
+    'Validator',
+    'ValidatorStatus',
+    'BlockProposal',
+    'Vote',
+    'VoteType',
+    'ABCIApplication'
+]
