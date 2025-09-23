143fb33187004d3ecb5a8322ffddbacded2ee431
RayoniR <rayonireuben@duck.com>
2025-09-23 08:13:46 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- blockchain/models/__init__.py
+++ blockchain/models/__init__.py
@@ -1,14 +1,14 @@
 # blockchain/models/__init__.py
-from blockchain.models.validation import ValidationLevel(Enum)
-from blockchain.models.block import BlockVersion(Enum)
+from blockchain.models.validation import ValidationLevel
+from blockchain.models.block import BlockVersion
 from blockchain.models.chain_state import ChainState
 from blockchain.models.fork import ForkResolution
 from blockchain.models.transaction_results import TransactionCreationResult
 
 __version__ = "1.0.0"
 __all__ = [
-    'ValidationLevel(Enum)',
-    'BlockVersion(Enum)',
+    'ValidationLevel',
+    'BlockVersion',
     'ChainState',
     'ForkResolution',
     'TransactionCreationResult'
