823c6e4c16c81a13f89f4ddfaf3fe7d38ca73a11
RayoniR <rayonireuben@duck.com>
2025-09-23 16:39:11 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- smart_contract/core/__init__.py
+++ smart_contract/core/__init__.py
@@ -2,8 +2,10 @@
 from smart_contract.core.contract import SmartContract
 from smart_contract.core.contract_manager import ContractManager
 from smart_contract.core.execution_result import ExecutionResult
-from smart_contract.core.gas_system import GasMeter, GasOptimizer, OutOfGasError
-from smart_contract.core.storage import ContractStorage
+from smart_contract.core.gas_system.gas_meter import GasMeter 
+from smart_contract.core.gas_system.gas_optimizer import GasOptimizer
+from smart_contract.core.gas_system.out_of_gas_error import OutOfGasError
+from smart_contract.core.storage.contract_storage import ContractStorage
 
 __all__ = [
     'SmartContract', 'ContractManager', 'ExecutionResult',
