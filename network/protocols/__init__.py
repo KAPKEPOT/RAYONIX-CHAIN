2f13443529298fc752b5410dfed7631101d3839f
RayoniR <rayonireuben@duck.com>
2025-10-19 08:21:11 +0300

    Add files via upload
    Signed-off-by: RayoniR <rayonireuben@duck.com>

--- network/protocols/__init__.py
+++ network/protocols/__init__.py
@@ -1,7 +1,6 @@
 from network.protocols.tcp_handler import TCPHandler
-from network.protocols.udp_handler import UDPHandler
 from network.protocols.websocket_handler import WebSocketHandler
 from network.protocols.http_handler import HTTPHandler
-from network.protocols.udp_protocol import UDPProtocol
+
 
 __all__ = ['TCPHandler', 'UDPHandler', 'WebSocketHandler', 'HTTPHandler', 'UDPProtocol']
