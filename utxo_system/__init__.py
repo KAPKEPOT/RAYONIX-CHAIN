bc48f0cf900dc39e75a6f1cc491baa7992c6649d
rayonix <rayonireuben@duck.com>
2025-11-02 15:37:36 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- utxo_system/__init__.py
+++ utxo_system/__init__.py
@@ -1,6 +1,6 @@
 # utxo_system/__init__.py
 from utxo_system.models import UTXO, Transaction
-from utxo_system.database import UTXOSet
+from utxo_system.core import UTXOSet
 from utxo_system.crypto import sign_transaction_input, verify_transaction_signature
 from utxo_system.validation import validate_transaction
 from utxo_system.exceptions import SerializationError, DeserializationError, SerializationError, ValidationError
