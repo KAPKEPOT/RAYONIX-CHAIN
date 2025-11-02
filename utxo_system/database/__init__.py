85ed41325dfa7e3a85082c5168467092e6bf9544
rayonix <rayonireuben@duck.com>
2025-11-02 15:35:30 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- utxo_system/database/__init__.py
+++ utxo_system/database/__init__.py
@@ -1,6 +1,5 @@
 # utxo_system/database/__init__.py
-from utxo_system.database.core import UTXOSet
 from utxo_system.database.indexing import AddressIndexer
 from utxo_system.database.serialization import serialize_utxo, deserialize_utxo
 
-__all__ = ['UTXOSet', 'AddressIndexer', 'serialize_utxo', 'deserialize_utxo']
+__all__ = [ 'AddressIndexer', 'serialize_utxo', 'deserialize_utxo']
