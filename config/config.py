b65ecfd81b510456b801bd97e8fb17657e4b4666
rayonix <rayonireuben@duck.com>
2025-11-02 18:40:41 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- config/config.py
+++ config/config.py
@@ -4,6 +4,7 @@ from typing import List, Optional, Dict, Any
 import yaml
 import os
 from pathlib import Path
+from network.config.network_types import NetworkType
 
 @dataclass
 class NetworkConfig:
