fdfacbf1fc3e57899da7f59a6e17dd776e04b337
rayonix <rayonireuben@duck.com>
2025-11-06 17:12:49 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- network/interfaces/discovery_interface.py
+++ network/interfaces/discovery_interface.py
@@ -1,25 +1,32 @@
-from abc import ABC, abstractmethod
-from typing import List, Dict
+# network/interfaces/discovery_interface.py
+import abc
+from typing import List, Dict, Any
+from network.models.peer_info import PeerInfo
 
-class IPeerDiscovery(ABC):
-    """Interface for peer discovery mechanisms"""
+class IPeerDiscovery(abc.ABC):
+    """Interface for peer discovery implementations"""
     
-    @abstractmethod
+    @abc.abstractmethod
     async def discover_peers(self) -> List[Dict]:
         """Discover new peers"""
         pass
     
-    @abstractmethod
+    @abc.abstractmethod
     async def bootstrap_network(self):
-        """Bootstrap to the network"""
+        """Bootstrap the network"""
         pass
     
-    @abstractmethod
+    @abc.abstractmethod
     async def maintain_peer_list(self):
-        """Maintain and update peer list"""
+        """Maintain peer list"""
         pass
     
-    @abstractmethod
+    @abc.abstractmethod
     async def request_peer_lists(self):
         """Request peer lists from connected peers"""
+        pass
+    
+    @abc.abstractmethod
+    def add_peer(self, peer_info: PeerInfo):
+        """Add a peer"""
         pass
