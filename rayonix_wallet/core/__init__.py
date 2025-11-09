e93d2e18198d9dfa9c579bfdd66b7e4265d7b0ca
rayonix <rayonireuben@duck.com>
2025-11-09 12:14:07 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- rayonix_wallet/core/__init__.py
+++ rayonix_wallet/core/__init__.py
@@ -1,7 +1,7 @@
 from rayonix_wallet.core.wallet import RayonixWallet
 from rayonix_wallet.core.config import WalletConfig
 from rayonix_wallet.core.wallet_types import WalletType, KeyDerivation, AddressType
-from rayonix_wallet.core.types import SecureKeyPair, Transaction, AddressInfo, WalletBalance, WalletState
+from rayonix_wallet.core.wallet_types import SecureKeyPair, Transaction, AddressInfo, WalletBalance, WalletState
 from rayonix_wallet.core.exceptions import WalletError, DatabaseError, CryptoError, SyncError
 
 __all__ = [
