e9d4cbaee33f9f118c9023863231d5bf658ddea9
rayonix <rayonireuben@duck.com>
2025-11-10 21:21:17 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- rayonix_node/cli/__init__.py
+++ rayonix_node/cli/__init__.py
@@ -1,6 +1,1 @@
-# rayonix_node/cli/wallet_commands/__init__.py
-
-from rayonix_node.cli.wallet_commands.create_wallet import CreateWalletCommand
-from rayonix_node.cli.wallet_commands.load_wallet import LoadWalletCommand
-
-__all__ = ['CreateWalletCommand', 'LoadWalletCommand']
+# cli/__init__.py - Command-line interface package initialization
