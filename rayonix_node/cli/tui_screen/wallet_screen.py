07b5447559aa321f330a72c7fbc9b87afacc432f
rayonix <rayonireuben@duck.com>
2025-11-13 22:59:12 +0300

    Add files via upload
    Signed-off-by: rayonix <rayonireuben@duck.com>

--- rayonix_node/cli/tui_screen/wallet_screen.py
+++ rayonix_node/cli/tui_screen/wallet_screen.py
@@ -75,7 +75,7 @@ class WalletScreen(Screen):
     }
     """
     
-    def __init__(self, rpc_client, app):
+    def __init__(self, rpc_client):
         super().__init__()
         self.client = rpc_client
         #self.app = app
